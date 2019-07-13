829ebf7f64ae30ddaa50bcfcc692ae59bcf78735
KDanila <bus1d0@mail.ru>
2019-07-13 21:54:12 +0400

    Ten min walk

---
+++ src/test/java/tenMinuteWalk/TenMinWalkTest.java
@@ -0,0 +1,22 @@
+package tenMinuteWalk;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * TenMinWalkTest class.
+ */
+public class TenMinWalkTest {
+    /**
+     * Simple test.
+     */
+    @Test
+    public void Test() {
+        assertEquals("Should return true", true, TenMinWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s'}));
+        assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}));
+        assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'w'}));
+        assertEquals("Should return false", false, TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}));
+    }
+}
+
+
